742d8683a4474d6c0de98eae883e23381ea1c536
paulo_uce <aldazpaulo@gmail.com>
2023-12-07 19:45:04 -0500

    cdn vue

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("Elementos Vue");
+console.log(Vue);
+
+const app = Vue.createApp({
+//   template: `
+//     <h1> Hola Mundo</h1>
+//     <p>Vue.js con CDN</p>
+    
+//     <h1>{{1+1}}</h1>
+//     <p> {{[1,2,3,4,5,6,7,8]}}</p>
+//     <p> {{false ? 'Verdadero' : 'Falso'}}</p>
+//     `,
+});
+
+app.mount("#myApp");
