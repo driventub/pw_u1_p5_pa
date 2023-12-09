e7ee15c86bd076f7a62c4c52a4a44564fa88b673
paulo_uce <aldazpaulo@gmail.com>
2023-12-09 10:36:37 -0500

    propiedades reactivas

--- js/app.js
+++ js/app.js
@@ -3,13 +3,29 @@ console.log(Vue);
 
 const app = Vue.createApp({
 //   template: `
-//     <h1> Hola Mundo</h1>
-//     <p>Vue.js con CDN</p>
-    
-//     <h1>{{1+1}}</h1>
-//     <p> {{[1,2,3,4,5,6,7,8]}}</p>
-//     <p> {{false ? 'Verdadero' : 'Falso'}}</p>
-//     `,
+//       <h1> Hola Mundo</h1>
+//       <p>Vue.js con CDN</p>
+
+//       <h1>{{1+1}}</h1>
+//       <p> {{[1,2,3,4,5,6,7,8]}}</p>
+//       <p> {{false ? 'Verdadero' : 'Falso'}}</p>
+//       `,
+  data() {
+    return {
+      mensaje: "Hello",
+      valor: 1000
+    };
+  },
+
+  methods: {
+    cambiar(){
+        console.log("Se esta cambiando el mensaje");
+        this.mensaje = "Nuevo mensaje";
+    },
+    sumar(){
+        this.valor += 1
+    }
+  },
 });
 
 app.mount("#myApp");
