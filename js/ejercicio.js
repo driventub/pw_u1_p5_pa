41e75ab9ec5eb531350e20c4df80f4c58fceddc0
driven <aldazpaulo@gmail.com>
2023-12-19 23:52:07 -0500

    form con vue

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,31 +1,22 @@
-console.log("Elementos Vue");
-console.log(Vue);
 
 const estudiantes = [
-//   { nombre: "Perico", apellido: "Palotes" },
-//   { nombre: "Daniel", apellido: "Aquino" },
-//   { nombre: "Alison", apellido: "Guzman" },
-//   { nombre: "Tomas", apellido: "Guaman" },
-//   { nombre: "Agustin", apellido: "Rea" },
+  // { nombre: "Perico", apellido: "Palotes" , edad: 0},
+  // { nombre: "Daniel", apellido: "Aquino" , edad: 0},
+  // { nombre: "Alison", apellido: "Guzman" , edad: 0},
+  // { nombre: "Tomas", apellido: "Guaman" , edad: 0},
+  // { nombre: "Agustin", apellido: "Rea" , edad: 0},
 ];
 
 const app = Vue.createApp({
-  //   template: `
-  //       <h1> Hola Mundo</h1>
-  //       <p>Vue.js con CDN</p>
-
-  //       <h1>{{1+1}}</h1>
-  //       <p> {{[1,2,3,4,5,6,7,8]}}</p>
-  //       <p> {{false ? 'Verdadero' : 'Falso'}}</p>
-  //       `,
 
   data() {
     return {
-      mensaje: "Hello",
-      valor: 1000,
       lista: estudiantes,
       nombre: undefined,
       apellido: null,
+      edad: null,
+      genero: null,
+      pais: null,
       editar: "Editar",
     };
   },
@@ -40,27 +31,24 @@ const app = Vue.createApp({
     },
     agregar() {
       console.log("Agregando Estudiante...........");
-      const enviar = { nombre: this.nombre, apellido: this.apellido };
+      console.log(estudiantes);
+      const enviar = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad,
+        genero: this.genero,
+        pais: this.pais,
+      };
       this.lista.push(enviar);
+      
     },
     quitar(valor) {
       console.log(valor);
       this.lista.splice(valor, 1);
       this.editar = "Borrar";
     },
-    
-    presionarTecla({charCode}){
 
-      if (charCode == 13) {
-        this.agregar();
-      }
-
-      
-    },
-
-    presionarTeclaMod(){
-      console.log("presionando 2");
-    }
+   
   },
 });
 
